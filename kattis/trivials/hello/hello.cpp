a3190aedfc3720db23c188431552f57430851ff0
Võ Hồng Khanh <khanhvohong@gmail.com>
2017-11-08 10:20:44 +0700

    Create hello.cpp

---
+++ kattis/trivials/hello/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Hello World!
+Input
+There is no input for this problem.
+
+Output
+Output should contain one line, containing the string "Hello World!".
+*/
+
+int main()
+{
+    cout << "Hello world!";
+    return 0;
+}
